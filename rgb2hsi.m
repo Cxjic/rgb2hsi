894cfbf7b20c1fed9d28afc2d56e9ff9655a2392
chensonglu <chenslvs@163.com>
2015-12-29 10:25:21 +0800

    v1.02

--- rgb2hsi.m
+++ rgb2hsi.m
@@ -19,7 +19,7 @@ b = rgb(:, :, 3);
 % implement the conversion
 i = (r + g + b)/3;
 s = 1 - (min(min(r, g), b)./i);
-h = acos(0.5*(2*r - g - b)./sqrt((r - g)^2 + (r - b)*(g - b)));
+h = acos(0.5*(2*r - g - b)./sqrt((r - g).^2 + (r - b).*(g - b)));
 
 % concatenate h, s, i
 hsi = cat(3, h, s, i);
